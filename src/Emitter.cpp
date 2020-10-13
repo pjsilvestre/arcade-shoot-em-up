4be6d30e9978e928c0efeaa31270a25bcca6d824
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-10-13 02:07:17 -0700

    Add lifespan, position, direction, to Emitter

--- src/Emitter.cpp
+++ src/Emitter.cpp
@@ -1,15 +1,22 @@
 #include "Emitter.h"
 
 /**
+ * @brief Create an Emitter with no associated Sprite
+ */
+Emitter::Emitter() : lifespan{Constants::LIFESPAN} {}
+
+/**
  * @brief Create an Emitter with an associated Sprite
  * @param sprite The desired Sprite
  */
-Emitter::Emitter(const Sprite& sprite) : sprite{sprite} {}
+Emitter::Emitter(const Sprite& sprite) : Emitter() { this->sprite = sprite; }
 
 /**
  * @brief Update the associated ActorSystem
  */
-void Emitter::update() { actors.update(); }
+void Emitter::update() { 
+  actors.update(); 
+}
 
 /**
  * @brief Draw the associated ActorSystem
@@ -21,9 +28,11 @@ void Emitter::draw() { actors.draw(); }
  */
 void Emitter::emit() {
   Actor missile = Actor(sprite);
-  missile.setPosition(glm::vec3(1280 / 2, 720 / 2, 1));
-  missile.setLifespan(500);
+  missile.setPosition(position);
+  missile.setLifespan(lifespan);
   actors.add(missile);
 
-  actors.moveActors(glm::vec3(0, -1, 0));
+  actors.update();
+
+  actors.moveActors(glm::normalize(direction));
 }
--- src/Emitter.h
+++ src/Emitter.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #include "ActorSystem.h"
+#include "Constants.h"
 
 class Emitter {
  public:
-  Emitter(){};
+  Emitter();
   Emitter(const Sprite& sprite);
 
   void update();
   void draw();
 
+  void setLifespan(const int lifespan) { this->lifespan = lifespan; }
+  void setPosition(const glm::vec3& position) { this->position = position; }
+  void setDirection(const glm::vec3& direction) { this->direction = direction; }
+
   void emit();
 
  private:
+  int lifespan;
+  glm::vec3 position;
+  glm::vec3 direction;
+
   Sprite sprite;
   ActorSystem actors;
 };

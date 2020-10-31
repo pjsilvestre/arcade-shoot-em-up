cd95647b4cb4637aba92592694e6f2f200b4ea0d
Patrick <patrickjohnsilvestre@gmail.com>
2020-10-31 03:18:10 -0700

    Update Turret Integration_Strategy_Type

--- src/Turret.cpp
+++ src/Turret.cpp
@@ -1,12 +1,12 @@
 #include "Turret.h"
 
-
 /**
  * @brief Creates a Turret with the default missile sprite
  */
 Turret::Turret() : Emitter() {
   lifespan = Constants::MISSILE_LIFESPAN;
   magnitude = Constants::MISSILE_MAGNITUDE;
+  integrationStrategyType = Integration_Strategy_Type::turret;
   direction = glm::vec3(0.0f, -1.0f, 0.0f);
 
   Sprite missileSprite;
